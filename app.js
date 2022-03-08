4417e622ec2000f1890a1d758d2571acb3dd82d8
Vhagelid <90680002+Vhagelid@users.noreply.github.com>
2022-03-08 11:54:58 +0100

    Functions

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const sessionsRouter = require("./src/routers/sessionsRouter");
 const adminRouter = require("./src/routers/adminRouter");
+const topMoviesRouter = require("./src/routers/topMoviesRouter");
 
 
 
@@ -22,6 +23,7 @@ app.set("view engine", "ejs");
 
 app.use("/sessions", sessionsRouter);
 app.use("/admin", adminRouter);
+app.use("/movies", topMoviesRouter);
 
 app.get("/",(req, res)=>{
     res.render("index", {title: "Globomantics", data: ["a", "b", "c"]});
