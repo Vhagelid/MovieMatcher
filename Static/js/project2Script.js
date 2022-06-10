a895da847f83299ad51e206b3899de41f55423c2
Vhagelid <90680002+Vhagelid@users.noreply.github.com>
2022-06-10 08:58:58 +0200

    last changes

--- Static/js/project2Script.js
+++ Static/js/project2Script.js
@@ -132,3 +132,15 @@ function VisHytteRute(rute) {
 // }
 
 // makeList(listeMedHytter);
+
+var biler = [
+    [1, 2 ,3 ,4 ,5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14]
+]
+
+for (const numblist of biler) {
+    numblist.forEach(element => {
+        console.log(numblist[0]);
+    });
+}
